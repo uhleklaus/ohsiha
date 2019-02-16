08bdd196922a09991d8a31decfd201bcbac6f117
Klaus Uhle <klaus.uhle@gmail.com>
2019-02-16 13:54:20 +0200

    Tuki rekisteröitymiselle ja kirjautumiselle

--- familyHome/urls.py
+++ familyHome/urls.py
@@ -15,7 +15,20 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from userInput import views
+
+from userInput import views as user_views
+#from django.conf.urls import url
+from django.contrib.auth import views as auth_views
+
+from userInput import views as core_views
+
 
 urlpatterns = [
+    path('register/', core_views.register, name='register'),
+    path('', views.home, name= 'project-home'),
     path('admin/', admin.site.urls),
+    # path('register/', user_views.register, name= 'register'),
+    path('login/', auth_views.LoginView.as_view(template_name = 'userInput/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name = 'userInput/logout.html'), name='logout'),
 ]
--- userInput/views.py
+++ userInput/views.py
@@ -1,3 +1,29 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+from django.contrib.auth.forms import UserCreationForm
+
+#def register(request):
+ #   form = UserCreationForm()
+  #  return render (request, 'userInput/register.html', {'form' : form})
+
+def home(request):
+    return render (request, 'userInput/home.html')
+
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render, redirect
+
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password)
+            login(request, user)
+            return redirect('project-home')
+    else:
+        form = UserCreationForm()
+    return render(request, 'userInput/register.html', {'form': form})
